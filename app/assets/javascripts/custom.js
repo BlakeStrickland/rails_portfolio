1072b898552507869f0147375470acbfef271a44
Blake Strickland <Stricklandb02@gmail.com>
2016-07-05 15:29:35 -0400

    Remove excess

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,17 +1,3 @@
-/* -------------------------------------------------------------------------------
-=========================================
-SELENA - Free Multi-purpose One Page Template
-Version: 1.0 (Initial Release)
-Designed By: Maher Jarrah
-
-Website:		http://maherjarrah.com
-Twitter:			https://twitter.com/MaherJarrah1
-Facebook:		https://fb.com/maherjarrah111
-Behance: 		https://www.behance.net/maherjarrah
-
-You can use this template for your personal purposes only, and it's under Creative Commons Attribution-NonCommercial v3.0 license https://creativecommons.org/licenses/by-nc/3.0/
-=========================================
---------------------------------------------------------------------------------*/
 
 /*===================
 	 PRELOADER
@@ -29,59 +15,59 @@ jQuery(document).ready(function($) {
 	 Touch Fixes
 ===================*/
 env = (function() {
-	
+
 	var flags = {}, el = document.createElement('div'), root = document.documentElement, i
-	
+
 	function flag(names) {
 		names = names.split(' ')
 		for (i = 0; i < names.length; i++)
 		flags[names[i]] = true
 	}
-	
+
 	function classnames() {
 		var names = [], name
 		for(name in flags) if (flags.hasOwnProperty(name))
 		names.push(name)
 		root.className += (root.className ? ' ' : '') + names.join(' ')
 	}
-  
+
 	el.setAttribute('ontouchstart', 'return;')
 	if (typeof el.ontouchstart === 'function') flag('touch')
 	if (navigator.msPointerEnabled && navigator.msMaxTouchPoints > 1) flag('touch')
-  
+
 	classnames()
 	return flags
-	
+
 })()
 
 /*===================
 	 NAVIGATION
 ===================*/
 $(document).ready(function() {
-	
+
 	$('.navigation-links').onePageNav({
 		scrollThreshold: 0.2, // Percentage of screen at which the next section should become current
 		filter: ':not(.external)', // Selector of links to filter out of one page nav functionality.
 		easing: 'none', // fixing the bug
 		changeHash: true // Whether you want to change the hash when a user clicks on the navigation
 	});
-	
+
 });
 
 (function ($){
-	
+
 	$(".navigation-links").navigation({
 		maxWidth: "930px",
 		label: false
 	});
-	
+
 })(jQuery);
 
 /*===================
 	 RANGE INPUT
 ===================*/
 var range = $('.range-slider input'), value = $('.range-value');
-    
+
 value.html(range.attr('value'));
 
 range.on('input', function(){
@@ -92,19 +78,19 @@ range.on('input', function(){
 	 PORTFOLIO
 ===================*/
 (function ($){
-	
+
 	$('.projects').imagesLoaded(function() {
         var options = {
 			autoResize: true,
 			container: $('.projects-container'),
 			offset:0
         };
-		
+
 		var handler = $('.projects li');
 		filters = $('.portfolio-filters li');
 
 		handler.wookmark(options);
-		
+
 		/**
          * When a filter is clicked, toggle it's active state and refresh.
          */
@@ -127,5 +113,5 @@ range.on('input', function(){
 		// Capture filter click events.
 		filters.click(onClickFilter);
     });
-	
+
 })(jQuery);
